0b881fac203337c3292a54f9016c7d88e0d9873e
Christian <112215625+christianrocha383@users.noreply.github.com>
2023-03-12 20:31:18 -0300

    Update app.js

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,3 +1,7 @@
+/* 
+
+Exibindo quantos números abaixo de 501 o array abaixo possui.
+
 const crazyNumbers = [937, 5, 395, 402, 501, 333, 502, 781, 3, 691]
 
 const numeroDeArrays = crazyNumbers.reduce ((accumulator, numbers) =>{
