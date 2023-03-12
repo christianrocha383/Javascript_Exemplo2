fbb1ae02d745f9a7c8ea4fd8c3c160e22bbbd907
Christian <112215625+christianrocha383@users.noreply.github.com>
2023-03-12 18:06:06 -0300

    Update app.js

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,6 +1,5 @@
 const crazyNumbers = [937, 5, 395, 402, 501, 333, 502, 781, 3, 691]
 
-const numeroDeArrays = crazyNumbers.filter (numbers => numbers <501   
- )
+const numeroDeArrays = crazyNumbers.filter (numbers => numbers < 501)
 
 console.log(numeroDeArrays)
